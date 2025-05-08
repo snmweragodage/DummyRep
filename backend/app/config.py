1eefbda960995a75152d565784c21cf3703a8ace
snmweragodage <sujininilhara123@gmail.com>
2025-05-08 01:52:56 +0100

    Remove backend files

--- backend/app/config.py
+++
@@ -1,3 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://skinuser:strong_password@localhost/skincancerdb'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-UPLOAD_FOLDER = 'uploads'
--- backend/app/database.py
+++
@@ -1,6 +0,0 @@
-from models import db
-
-def init_db(app):
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
